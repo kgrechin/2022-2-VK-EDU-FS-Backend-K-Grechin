9c6e9f436763b6edc70ae44ddec0ae6aa13ee0a8
grechin-kirill <grechin.ka@yandex.ru>
2022-09-28 12:23:21 +0300

    refactor homework_1
    - папка проекта переименована на homework_1
    - приватные поля изменены на публичные
    - однострочники заменены на обычные циклы
    - добавлена обработка разных ситуаций исключений при вводе
    
    -

--- homework_1/test_tic_tac_game.py
+++ homework_1/test_tic_tac_game.py
@@ -13,7 +13,7 @@ class TicTacGameTest(unittest.TestCase):
 
     def setUp(self):
         self.game = TicTacGame()
-        self.game._TicTacGame__init_game()
+        self.game.init_game()
 
     def tearDown(self):
         self.game = None
@@ -24,93 +24,74 @@ class TicTacGameTest(unittest.TestCase):
         board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.assertEqual(self.game.board, board)
 
-        available_celss = set(board)
-        self.assertEqual(self.game.available_cells, available_celss)
-
-    @patch('builtins.print')
-    def test_show_board(self, mock_print):
-        '''Тестирование функции show_board'''
-
-        show_board = '1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9'
-
-        self.game._TicTacGame__show_board()
-        mock_print.assert_called_with(show_board)
+        available_cells = set(board)
+        self.assertEqual(self.game.available_cells, available_cells)
 
     def test_check_winner_default(self):
         '''Тестирование функции check_winner на default значения'''
 
-        check_winner = self.game._TicTacGame__check_winner
-
-        self.assertEqual(None, check_winner(self.X_SIGN))
-        self.assertEqual(None, check_winner(self.O_SIGN))
+        self.assertIsNone(self.game.check_winner(self.X_SIGN))
+        self.assertIsNone(self.game.check_winner(self.O_SIGN))
 
     def test_check_winner_x(self):
         '''Тестирование функции check_winner на победу крестиков'''
 
-        check_winner = self.game._TicTacGame__check_winner
-
-        self.game._TicTacGame__board = ['x', 'x', 'x',
-                                        'o', '5', '6',
-                                        'o', 'o', '9']
+        self.game.board = ['x', 'x', 'x',
+                           'o', '5', '6',
+                           'o', 'o', '9']
 
         with self.assertRaises(StopIteration) as context:
-            check_winner(self.X_SIGN)
+            self.game.check_winner(self.X_SIGN)
+
         self.assertEqual("Победили x'ки", str(context.exception))
 
     def test_check_winner_o(self):
         '''Тестирование функции check_winner на победу ноликов'''
 
-        check_winner = self.game._TicTacGame__check_winner
-
-        self.game._TicTacGame__board = ['x', '2', 'o',
-                                        'x', 'o', '6',
-                                        'o', 'o', 'x']
+        self.game.board = ['x', '2', 'o',
+                           'x', 'o', '6',
+                           'o', 'o', 'x']
 
         with self.assertRaises(StopIteration) as context:
-            check_winner(self.O_SIGN)
+            self.game.check_winner(self.O_SIGN)
+
         self.assertEqual("Победили o'ки", str(context.exception))
 
     def test_check_winner_draw(self):
         '''Тестирование функции check_winner на ничью'''
 
-        check_winner = self.game._TicTacGame__check_winner
-
-        self.game._TicTacGame__available_cells = set()
+        self.game.available_cells = set()
 
         with self.assertRaises(StopIteration) as context:
-            check_winner(self.X_SIGN)
+            self.game.check_winner(self.X_SIGN)
+
         self.assertEqual("Ничья", str(context.exception))
 
     @patch('builtins.input', side_effect=['1', '20', 'z'])
-    def test_validate_input(self, mock_input):
+    def test_validate_input(self, _):
         '''Тестирование функции validate_input'''
 
-        validate_input = self.game._TicTacGame__validate_input
-
-        self.assertEqual('1', validate_input(self.X_SIGN))
-        self.assertRaises(ValueError, validate_input, self.X_SIGN)
-        self.assertRaises(ValueError, validate_input, self.X_SIGN)
+        self.assertEqual(0, self.game.validate_input(self.X_SIGN))
+        self.assertRaises(ValueError, self.game.validate_input, self.X_SIGN)
+        self.assertRaises(ValueError, self.game.validate_input, self.X_SIGN)
 
     @patch('builtins.input', return_value='1')
-    @patch('builtins.print')
-    def test_make_move(self, mock_print, mock_input):
+    def test_make_move(self, _):
         '''Тестирование функции make_move'''
 
-        self.assertEqual(len(self.game.available_cells), len(self.game.board))
-
-        self.game._TicTacGame__make_move(self.X_SIGN)
+        av_cells_len = len(self.game.available_cells)
+        self.game.make_move(self.X_SIGN)
 
         board = ['x', '2', '3',
                  '4', '5', '6',
                  '7', '8', '9']
 
         self.assertEqual(self.game.board, board)
-        self.assertEqual(len(self.game.available_cells),
-                         len(self.game.board) - 1)
+        self.assertEqual(av_cells_len - 1, len(self.game.available_cells))
 
     @patch('builtins.input', side_effect=['1', '2', '5', '3', '9'])
     @patch('builtins.print')
-    def test_game_winner_x(self, mock_print, mock_input):
+    def test_game_winner_x(self, mock_print, _):
         '''Тестирование функции start_game на победу крестиков'''
 
         self.game.start_game()
@@ -118,7 +99,7 @@ class TicTacGameTest(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['1', '2', '3', '5', '7', '8'])
     @patch('builtins.print')
-    def test_game_winner_o(self, mock_print, mock_input):
+    def test_game_winner_o(self, mock_print, _):
         '''Тестирование функции start_game на победу ноликов'''
 
         self.game.start_game()
@@ -126,7 +107,7 @@ class TicTacGameTest(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['1', '2', '4', '7', '3', '5', '6', '9', '8'])
     @patch('builtins.print')
-    def test_game_winner_draw(self, mock_print, mock_input):
+    def test_game_winner_draw(self, mock_print, _):
         '''Тестирование функции start_game на ничью'''
 
         self.game.start_game()
---
+++ homework_1/tic_tac_game.py
@@ -0,0 +1,90 @@
+'''Модуль реализации игры в крестики-нолики'''
+
+
+class TicTacGame:
+    '''Класс игры в крестики-нолики'''
+
+    SIZE = 3
+
+    WIN_TACTICS = (
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),
+        (0, 4, 8), (2, 4, 6))
+
+    X_SIGN, O_SIGN = 'x', 'o'
+
+    def __init__(self):
+        self.board = None
+        self.available_cells = None
+
+    def init_game(self):
+        '''Функция инициализации игровой доски'''
+
+        self.board = [str(i) for i in range(1, self.SIZE ** 2 + 1)]
+        self.available_cells = set(self.board)
+
+    def start_game(self):
+        '''Функция для старта игры'''
+
+        while True:
+            try:
+                self.make_move(self.X_SIGN)
+                self.make_move(self.O_SIGN)
+            except ValueError as error:
+                print(error)
+            except StopIteration as stop:
+                print(stop.value)
+                break
+
+    def make_move(self, sign):
+        '''Функция выполнения хода в игре'''
+
+        self.show_board()
+
+        num = self.validate_input(sign)
+
+        self.board[num] = sign
+        self.available_cells.discard(str(num + 1))
+
+        self.check_winner(sign)
+
+    def show_board(self):
+        '''Функция печати игровой доски'''
+
+        for i in range(0, self.SIZE ** 2, self.SIZE):
+
+            row = self.board[i:i + self.SIZE]
+
+            print(' | '.join(row), '----------', sep='\n')
+
+    def validate_input(self, sign):
+        '''Функция валидации пользовательского ввода'''
+
+        answer = input(f'Куда поставить {sign}? ')
+
+        if not answer.isdigit():
+            raise ValueError('Значение должно быть цифрой')
+
+        if answer not in self.available_cells:
+            raise ValueError('Цифра должна быть свободна на доске')
+
+        return int(answer) - 1
+
+    def check_winner(self, sign):
+        '''Функция определения победителя'''
+
+        if len(self.available_cells) == 0:
+            raise StopIteration('Ничья')
+
+        for tactic in self.WIN_TACTICS:
+
+            is_row_win = map(lambda i: self.board[i] == sign, tactic)
+
+            if all(is_row_win):
+                raise StopIteration(f"Победили {sign}'ки")
+
+
+if __name__ == '__main__':
+    game = TicTacGame()
+    game.init_game()
+    game.start_game()
