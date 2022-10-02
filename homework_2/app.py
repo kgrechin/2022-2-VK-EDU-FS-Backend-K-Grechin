c740f037a41242970dd5122c5c75ceecfe776126
grechin-kirill <kirillgrechin8080@gmail.com>
2022-10-02 15:41:20 +0300

    homework_2 done

---
+++ homework_2/app.py
@@ -0,0 +1,10 @@
+'''simple gunicorn app'''
+
+def app(_, start_response):
+    '''hello world function'''
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
